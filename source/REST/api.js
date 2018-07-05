44f055c70353f1c8e34843debc4853dd4ca2ec8d
Andrey Smirnov <iadisey@gmail.com>
2018-07-06 00:36:38 +0300

    Make API

--- source/REST/api.js
+++ source/REST/api.js
@@ -1,2 +1,95 @@
+import { MAIN_URL, TOKEN } from './config';
 export const api = {
+    async fetchTasks () {
+        const response = await fetch(MAIN_URL, {
+            method:  "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization:  TOKEN,
+            },
+        });
+
+        if (response.status !== 200) {
+            console.error(`Error response - `, response);
+            throw new Error("Tasks Where not loaded");
+        }
+        const { data: tasks } = await response.json();
+
+        return tasks;
+    },
+    async createTask (taskName) {
+        const response = await fetch(MAIN_URL, {
+            method:  "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization:  TOKEN,
+            },
+            body: JSON.stringify({ message: taskName }),
+        });
+
+        if (response.status !== 200) {
+            console.error(`Error response - `, response);
+            throw new Error("Tasks Where not Create");
+        }
+        const { data: task } = await response.json();
+
+
+        return task;
+    },
+    async updateTask (updTask) {
+        // console.log(`putTask received - `, updTask);
+        const response = await fetch(MAIN_URL, {
+            method:  "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization:  TOKEN,
+            },
+            body: JSON.stringify(updTask),
+        });
+
+        if (response.status !== 200) {
+            console.error(`Error response - `, response);
+            throw new Error("Tasks Where not Update");
+        }
+        const { data: tasks } = await response.json();
+
+        return tasks;
+    },
+
+    async completeAllTasks  (updTasks) {
+        await Promise.all(updTasks.map(async (updTask) => {
+
+            const response = await fetch(MAIN_URL, {
+                method:  "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization:  TOKEN,
+                },
+                body: JSON.stringify(updTask),
+            });
+
+            if (response.status !== 200) {
+                console.error(`Error response - `, response);
+                throw new Error("Tasks Where not Update");
+            }
+
+        })).then((results) => {
+            console.log(`Выполнен перевод всех задач в статус выполнено.${results}. А точнее все оновлены на сервере. ;)`);
+        });
+    },
+
+    async removeTask (id) {
+        const response = await fetch(`${MAIN_URL}/${id}`, {
+            method:  "DELETE",
+            headers: {
+                Authorization: TOKEN,
+            },
+        });
+
+        if (response.status !== 204) {
+            console.error(`Error response - `, response);
+            throw new Error("Task not delete");
+        }
+    },
+
 };
